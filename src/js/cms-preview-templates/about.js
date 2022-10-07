dd1a2add7be00b8437b3ccbf277ac8d821ccf3e8
Severin Bruhat <sbruhat@gmail.com>
2022-10-07 17:01:44 +0100

    adjust cms preview for about us

--- src/js/cms-preview-templates/about.js
+++ src/js/cms-preview-templates/about.js
@@ -2,13 +2,29 @@ import React from "react";
 
 export default class AboutPreview extends React.Component {
   render() {
-    const {entry, widgetFor, getAsset} = this.props;
+    const {entry, widgetFor} = this.props;
 
     return <div className="mw6 center ph3 pv4">
       <h1 className="f2 lh-title b mb3">{ entry.getIn(["data", "title"])}</h1>
       <div className="cms mw6">
         { widgetFor("body") }
       </div>
+      
+      <div className="bg-grey-1 pv4">
+          <div className="ph3 mw7 center">
+
+            <div className="flex-l mhn2-l">
+              <div className="w-40-l ph2-l">
+                <h2 className="f2 b lh-title mb2">TEST h2</h2>
+              </div>
+            </div>
+
+            <div className="tc">
+              <a href="#" className="btn raise">Read more</a>
+            </div>
+
+          </div>
+        </div>
     </div>;
   }
 }
